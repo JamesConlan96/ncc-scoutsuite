fe2605a050df7acca4a5b12152fabe814c2d3352
Jason Ross <algorythm@gmail.com>
2022-10-16 07:39:46 -0400

    set up ncc-scoutsuite:latest image to point to ncc-scoutsuite:base-latest

--- docker/tag.sh
+++ docker/tag.sh
@@ -10,6 +10,7 @@ docker tag rossja/ncc-scoutsuite:aws-${VERSION} rossja/ncc-scoutsuite:aws-latest
 docker tag rossja/ncc-scoutsuite:azure-${VERSION} rossja/ncc-scoutsuite:azure-latest
 docker tag rossja/ncc-scoutsuite:gcp-${VERSION} rossja/ncc-scoutsuite:gcp-latest
 docker tag rossja/ncc-scoutsuite:base-${VERSION} rossja/ncc-scoutsuite:base-latest
+docker tag rossja/ncc-scoutsuite:base-${VERSION} rossja/ncc-scoutsuite:latest
 
 docker push rossja/ncc-scoutsuite:aws-${VERSION}
 docker push rossja/ncc-scoutsuite:azure-${VERSION}
@@ -20,3 +21,4 @@ docker push rossja/ncc-scoutsuite:aws-latest
 docker push rossja/ncc-scoutsuite:azure-latest
 docker push rossja/ncc-scoutsuite:gcp-latest
 docker push rossja/ncc-scoutsuite:base-latest
+docker push rossja/ncc-scoutsuite:latest
